38fb6e40949ad95bcc2f1f4a6fc07f2edb497c42
solareenlo <solareenlo@gmail.com>
2021-05-10 02:56:32 +0900

    add: _put_result

--- framework/libunit.c
+++ framework/libunit.c
@@ -6,21 +6,35 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 10:14:40 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/09 21:13:41 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/10 02:19:36 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
 
+static int	_put_result(int total, int ok)
+{
+	if (total == ok)
+	{
+		printf("%s%d/%d tests checked%s\n", GREEN, ok, total, RESET);
+		return (0);
+	}
+	else
+	{
+		printf("%s%d/%d tests checked%s\n", RED, ok, total, RESET);
+		return (-1);
+	}
+}
+
 int	_run_tests(t_list_ut **testlist)
 {
 	int			total;
-	int			passed;
+	int			ok;
 	pid_t		pid;
 	t_list_ut	*head;
 
 	total = 0;
-	passed = 0;
+	ok = 0;
 	head = *testlist;
 	while (*testlist)
 	{
@@ -30,15 +44,12 @@ int	_run_tests(t_list_ut **testlist)
 		else if (pid == 0)
 			ut_run_child(*testlist);
 		else
-			ut_run_parent(*testlist, &passed);
+			ut_run_parent(*testlist, &ok);
 		total++;
 		*testlist = (*testlist)->next;
 	}
 	*testlist = head;
-	if (total == passed)
-		return (0);
-	else
-		return (-1);
+	return (_put_result(total, ok));
 }
 
 int	launch_tests(t_list_ut **testlist)
