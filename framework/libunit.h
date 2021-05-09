ede8ae79e7a6a1d6fc08f53b44e47a63dafe1e27
solareenlo <solareenlo@gmail.com>
2021-05-09 11:34:33 +0900

    update: ut_libft

--- framework/libunit.h
+++ framework/libunit.h
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 10:14:35 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/09 10:43:54 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/09 10:57:27 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,9 @@ t_list_ut	*ut_lstlast(t_list_ut *lst);
 void		ut_lstadd_back(t_list_ut **testlist, char *name,
 				int (*func)(void));
 size_t		ut_strlen(char *s);
+void		ut_putchar_fd(char c, int fd);
 void		ut_putstr_fd(char *s, int fd);
+void		ut_putendl_fd(char *s, int fd);
 int			ut_putstr_err(char *s);
 
 #endif
--- framework/ut_libft.c
+++ framework/ut_libft.c
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/09 10:21:08 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/05/09 10:44:38 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/05/09 10:57:00 by tayamamo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,26 @@ size_t	ut_strlen(char *s)
 	return (i);
 }
 
+void	ut_putchar_fd(char c, int fd)
+{
+	if (!c || fd < 0)
+		return ;
+	write(fd, &c, 1);
+}
+
 void	ut_putstr_fd(char *s, int fd)
 {
 	if (!s || fd < 0)
 		return ;
 	write(fd, s, ut_strlen(s));
+}
+
+void	ut_putendl_fd(char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	ut_putstr_fd(s, fd);
+	ut_putchar_fd('\n', fd);
 }
 
 int	ut_putstr_err(char *s)
