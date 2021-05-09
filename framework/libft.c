35e590afd012f6e13ff94f8e77cb9c612040c115
solareenlo <solareenlo@gmail.com>
2021-05-09 10:35:21 +0900

    add: libft

---
+++ framework/libft.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/09 10:21:08 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/09 10:31:14 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libunit.h"
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	if (!s || fd < 0)
+		return ;
+	write(fd, s, ft_strlen(s));
+}
+
+int	ft_putstr_err(char *s)
+{
+	ft_putstr_fd("Error: ", 2);
+	ft_putstr_fd(s, 2);
+	ft_putstr_fd("\n", 2);
+	return (1);
+}
