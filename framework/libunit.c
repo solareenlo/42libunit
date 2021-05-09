84712f753fbe46055c83006149e590891aaedf1a
solareenlo <solareenlo@gmail.com>
2021-05-09 10:35:41 +0900

    update: libunit

--- framework/libunit.c
+++ framework/libunit.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libunit.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/09 10:14:40 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/09 10:34:59 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libunit.h"
 
-int	launch_tests(t_ut_list **testlist)
+int	launch_tests(t_list_ut **testlist)
 {
 	int	res;
 
--- framework/libunit.h
+++ framework/libunit.h
@@ -1,13 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libunit.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/09 10:14:35 by tayamamo          #+#    #+#             */
+/*   Updated: 2021/05/09 10:34:49 by tayamamo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBUNIT_H
 # define LIBUNIT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
-typedef struct s_ut_list
+typedef struct s_list_ut
 {
-	char				*ut_name;
-	int					(*ut_func)(void);
-	struct s_ut_list	*next;
-}	t_ut_list;
+	char				*name;
+	int					(*func)(void);
+	struct s_list_ut	*next;
+}	t_list_ut;
+
+t_list_ut	*ft_lstnew_ut(char *name, int (*func)(void));
+t_list_ut	*ft_lstlast_ut(t_list_ut *lst);
+void		ft_lstadd_back_ut(t_list_ut **testlist, char *name,
+				int (*func)(void));
+size_t		ft_strlen(char *s);
+void		ft_putstr_fd(char *s, int fd);
+int			ft_putstr_err(char *s);
 
 #endif
